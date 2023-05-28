8d22b6fd1015290e420b31cc92f99b03ce62d65a
Guilherme Cruz <guilhermecruz@gmail.com>
2023-05-27 23:37:10 -0300

    Fix type name

--- src/app/components/Providers.tsx
+++ src/app/components/Providers.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import { SessionProvider } from 'next-auth/react';
 interface Props{
-    children: ReactNode;
+    children: React.ReactNode;
 }
 
 const Providers = ({children}:Props) => {
