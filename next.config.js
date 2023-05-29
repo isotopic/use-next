0d0f3d7564471d441f766e7ce373fc6e4db07502
Guilherme Cruz <guilhermecruz@gmail.com>
2023-05-29 10:15:03 -0300

    secret debug 4

--- next.config.js
+++ next.config.js
@@ -4,7 +4,10 @@ const nextConfig = {
     output: 'export',
     distDir: 'dist',
     */
-    images: { unoptimized: true }
+    images: { unoptimized: true },
+    env:{
+        NEXTAUTH_SECRET: '1a3d74d87be1ceb78fa6c7729d29d912'
+    }
 }
 
 module.exports = nextConfig
