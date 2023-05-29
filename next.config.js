98631ca63d6e210dfd919cd74e5f82a23a7ced6b
Guilherme Cruz <guilhermecruz@gmail.com>
2023-05-29 17:46:06 -0300

    another variation

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ const nextConfig = {
     */
     reactStrictMode: true,
     images: { unoptimized: true },
+    experimental: {
+        appdir: true
+    }
 }
 
 module.exports = nextConfig
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -15,7 +15,7 @@ const handler = NextAuth({
     providers: [
         CredentialsProvider({
             // The name to display on the sign in form (e.g. "Sign in with...")
-            name: "Credentials",
+            name: "Sign in",
             // `credentials` is used to generate a form on the sign in page.
             // You can specify which fields should be submitted, by adding keys to the `credentials` object.
             // e.g. domain, username, password, 2FA token, etc.
@@ -24,7 +24,7 @@ const handler = NextAuth({
               username: { label: "Username", type: "text", placeholder: "username" },
               password: { label: "Password", type: "password" }
             },
-            async authorize(credentials, req) {
+            async authorize(credentials) {
               // Add logic here to look up the user from the credentials supplied
               // const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
               
@@ -53,6 +53,12 @@ const handler = NextAuth({
             }
           })
     ],
+    session: {
+      strategy: "jwt",
+    },
+    pages: {
+      signIn: "/dynamic",
+    },
     callbacks: {
       async jwt({ token }) {
         token.userRole = "admin"
