bcbab8b1b3eac4cb1c3e2688c3ea85c305162853
Guilherme Cruz <guilhermecruz@gmail.com>
2023-05-30 08:20:34 -0300

    Menu refactor

--- src/app/components/Menu/Menu.tsx
+++ src/app/components/Menu/Menu.tsx
@@ -4,7 +4,6 @@ import styles from './menu.module.css';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 
-
 function isActive(path:string, src:string){
     return path == src.split('?')[0];
 }
@@ -12,27 +11,43 @@ function isActive(path:string, src:string){
 export default function Menu(){
     const pathname = usePathname();
     const items = [
-        {src:'/', name: 'Home'},
-        {src:'/search-params?id=1', name: 'Search params'},
-        {src:'/dynamic/1', name: 'Dynamic path'},
-        {src:'/file-conventions', name: 'File conventions'},
-        {src:'/client-side-fetching', name: 'Client-side fetching'},
-        {src:'/server-side-fetching', name: 'Server-side fetching'},
-        {src:'/restricted-client', name: 'Restricted on client-side'},
-        {src:'/restricted-server', name: 'Restricted on server-side'},
-        {src:'/restricted-server-role', name: 'Restricted on server-side with role'},
-    ]
+        {name: 'Home', items: [
+            {src:'/', name: 'Home'},
+        ]},
+        {name: 'Routing', items: [
+            {src:'/search-params?id=1', name: 'Search params'},
+            {src:'/dynamic/1', name: 'Dynamic path'},
+        ]},
+        {name: 'Layout', items: [
+            {src:'/file-conventions', name: 'File conventions'},
+        ]},
+        {name: 'Data handling', items: [
+            {src:'/client-side-fetching', name: 'Client-side fetching'},
+            {src:'/server-side-fetching', name: 'Server-side fetching'},
+        ]},
+        {name: 'Authentication', items: [
+            {src:'/restricted-client', name: 'Restricted on client-side'},
+            {src:'/restricted-server', name: 'Restricted on server-side'},
+            {src:'/restricted-server-role', name: 'Restricted on server-side with role'},
+        ]},
+    ];
+
     return (
         <div className={styles.menu}> 
-            <ul>
-                {items.map(item => {
-                    return (
-                        <li key={item.src} className={isActive(pathname, item.src) ? styles.active : ''}>
-                            <Link href={item.src}>{item.name}</Link>
-                        </li>
-                    )
-                })}
-            </ul>
+            {items.map((section, i) => {
+                return (<>
+                    {i > 0 && <span>{section.name}</span>}
+                    <ul>
+                        {section.items.map((item) => {
+                            return (
+                                <li key={item.src} className={isActive(pathname, item.src) ? styles.active : ''}>
+                                    <Link href={item.src}>{item.name}</Link>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </>)
+            })}
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
           <h2>Home <p>/page.tsx</p></h2>
           <p>File for home page on app root.</p>
         </div>
-        <h3>This is a collection of snippets for NextJS with an app directory.</h3>
-        <p>External data is fetched from <a target="_blank" href="https://dummyjson.com/docs">dummyjson.com</a>, including users and login information.</p>
+        <p>This application is a collection of snippets for NextJS 13+ covering basic features like routing, layouts, server components, data fetching variations and anthentication.</p>
+        <br/><p>Note: External data is loaded from <a target="_blank" href="https://dummyjson.com/docs">dummyjson.com</a>, including users and login information.</p>
       </div>
     </main>
   )
